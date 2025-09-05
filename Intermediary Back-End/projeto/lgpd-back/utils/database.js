49114341011000bbe959f5fad15d9f252d446888
IC12 <iaracamargos@hotmail.com>
2025-09-04 21:36:53 -0300

    :hammer: Ajuste no database.js

--- Intermediary Back-End/projeto/lgpd-back/utils/database.js
+++ Intermediary Back-End/projeto/lgpd-back/utils/database.js
@@ -1,21 +1,17 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-    'lgpd-database',
-    'postgres',
-    'postgres',
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: 5432,
-        define: {
-            timestamps: false
-        },
-        dialectOptions: {
-            // Adicione a linha abaixo com o caminho do seu socketPath. Precisa instalar o servidor PostgreSQL
-            socketPath: '/caminho/do/seu/socket.sock'
-          }
-    }
-);
+const sequelize = new Sequelize("lgpd-database", "postgres", "postgres", {
+  host: "localhost",
+  dialect: "postgres",
+  port: 5432,
+  define: {
+    timestamps: false,
+  },
+  dialectOptions: {
+    // Adicione a linha abaixo com o caminho do seu socketPath.
+    //socketPath: '/caminho/do/seu/socket.sock'
+    // O socketPath é usado mais em ambientes Linux/Mac quando você conecta via Unix socket em vez de TCP/IP.
+  },
+});
 
 export default sequelize;
